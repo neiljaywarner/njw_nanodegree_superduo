48b30e5b22fc5bf4761cfd10c774d4fcb56f4426
Neil Jay Warner <neilwarner@airstrip.com>
2016-02-06 19:39:40 -0600

    Switch to names instead of icons because many icons missing.

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/ScoresWidgetProvider.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/ScoresWidgetProvider.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import java.text.SimpleDateFormat;
@@ -42,14 +43,17 @@ public class ScoresWidgetProvider extends AppWidgetProvider {
             int home_goals = cursor.getInt(cursor.getColumnIndex(DatabaseContract.scores_table.HOME_GOALS_COL));
             int away_goals = cursor.getInt(cursor.getColumnIndex(DatabaseContract.scores_table.AWAY_GOALS_COL));
             String time = cursor.getString(cursor.getColumnIndex(DatabaseContract.scores_table.TIME_COL));
-
-
+            String homeTeamName  = cursor.getString(cursor.getColumnIndex(DatabaseContract.scores_table.HOME_COL));
+            String awayTeamName = cursor.getString(cursor.getColumnIndex(DatabaseContract.scores_table.AWAY_COL));
+            Log.e("NJW", "home/away" + homeTeamName + "/" + awayTeamName);
 
             views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
             views.setTextViewText(R.id.score_textview, Utilies.getScores(home_goals,away_goals));
             views.setTextViewText(R.id.time_textview, time);
+            views.setTextViewText(R.id.home_name, homeTeamName);
+            views.setTextViewText(R.id.away_name, awayTeamName);
 
-          //  views.setImageViewResource(R.id.home_crest, R.drawable.abc_btn_check_material);
+            views.setImageViewResource(R.id.home_crest, R.drawable.abc_btn_check_material);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
 
